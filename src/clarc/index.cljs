2660a685f0efcb7ac5a39f0406040c2e23544584
François De Serres <francois.deserres@gmail.com>
2018-02-11 10:18:37 +0800

    add index entrypoint2

---
+++ src/clarc/index.cljs
@@ -0,0 +1,8 @@
+(ns clarc.index
+  (:require
+   [clarc.core :as clarc]
+   [devcards.core :as dc :include-macros true :refer [defcard deftest]]
+   [sablono.core :as sab :include-macros true])
+  (:require-macros))
+
+(dc/start-devcard-ui!)
