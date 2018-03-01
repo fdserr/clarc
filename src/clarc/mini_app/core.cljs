dac563ba84150f5afb996090555e42663deafd13
François De Serres <francois.deserres@gmail.com>
2018-03-01 11:06:37 +0800

    app comments

--- src/clarc/mini_app/core.cljs
+++ src/clarc/mini_app/core.cljs
@@ -3,14 +3,17 @@
     [clarc.mini-app.app :as app]))
 
 (defn render-app
+  "Render the app if site exists."
   [store]
   (if-let [node (.getElementById js/document "main-app-area")]
     (.render js/ReactDOM (app/ui-app store) node)))
 
+;; app store with render on change
 (defonce app-store (add-watch (atom {})
                               :render
                               (fn [_ a _ _] (render-app a))))
 
+;; initialise only once
 (defonce _ (app/dispatch! app-store app/action-init))
 
 ;;;
