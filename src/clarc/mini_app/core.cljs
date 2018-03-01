4e51a57a17d157fca3f825119b17f6f89a890477
François De Serres <francois.deserres@gmail.com>
2018-03-01 10:51:20 +0800

    app debugging

--- src/clarc/mini_app/core.cljs
+++ src/clarc/mini_app/core.cljs
@@ -12,3 +12,20 @@
                               (fn [_ a _ _] (render-app a))))
 
 (defonce _ (app/dispatch! app-store app/action-init))
+
+;;;
+
+(comment ;; enter at the REPL
+
+ (in-ns 'clarc.mini-app.core)
+
+ (add-watch app-store :watch
+            #(println "WATCH :value "
+                      "old=" (:value %2)
+                      " new=" (:value %3)))
+
+ (swap! app-store assoc :value 10000)
+
+ (app/dispatch! app-store app/action-init)
+
+ "")
